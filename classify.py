fe63e1ab29c2c2e3a67dd2b2b6336fe6bffe8a46
dylan-thinnes <dylan.thinnes@protonmail.com>
2019-02-22 17:35:03 +0000

    Add RGB channel extraction funcs

--- classify.py
+++ classify.py
@@ -4,3 +4,11 @@ import imageio
 
 def get_center(image):
     return
+
+# Obtaining individual RGB channels
+def get_r(original):
+    return np.dot(original, [1,0,0])
+def get_g(original):
+    return np.dot(original, [0,1,0])
+def get_b(original):
+    return np.dot(original, [0,0,1])
