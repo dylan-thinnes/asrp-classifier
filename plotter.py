6fa79107cb8b0628f2b75282272da55bed9f647c
dylan-thinnes <dylan.thinnes@protonmail.com>
2019-02-23 14:51:50 +0000

    Write functions to plot rgb as 3d vectors w/ color

--- plotter.py
+++ plotter.py
@@ -3,3 +3,18 @@ import numpy as np
 from mpl_toolkits.mplot3d import proj3d
 from scipy.cluster.hierarchy import dendrogram, linkage
 
+def plot_vector(ax, x, y, z):
+    ax.plot([0,x],[0,y],[0,z], color=(x / 255, y / 255, z / 255))
+
+def plot_vectors(vectors):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for vector in vectors:
+        plot_vector(ax, *vector)
+
+    ax.set_xlim([0,256])
+    ax.set_ylim([0,256])
+    ax.set_zlim([0,256])
+    plt.show()
+
